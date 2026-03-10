eb2ce8f840fe1689c775e7598b40e862668facc5
jwhuettl <jwhuettl@gmail.com>
2026-03-10 11:21:20 -0500

    Removing some js console messages that are no longer needed

--- javascripts/discourse/api-initializers/body-class.js
+++ javascripts/discourse/api-initializers/body-class.js
@@ -4,12 +4,6 @@ export default apiInitializer((api) => {
   api.onPageChange(() => {
     document.body.classList.add("sparq-theme");
 
-    console.log('before refresh');
-
-    if (document.querySelector('.page-not-found') && !Discourse.User.current()) {
-      console.log('refresh here');
-      window.location.href = '/login';
-    }
   });
 
   
